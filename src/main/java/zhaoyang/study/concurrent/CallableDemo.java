dcafdedeafe1cba0b7f6e9deda62e2f2d9555d54
zhaoyang666go <zhaoyang8216@sina.com>
2020-04-03 20:12:09 +0800

    线程类之实现Callable接口

---
+++ src/main/java/zhaoyang/study/concurrent/CallableDemo.java
@@ -0,0 +1,26 @@
+package zhaoyang.study.concurrent;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+class MyThread implements Callable<Integer> {
+
+    @Override
+    public Integer call() throws Exception {
+        System.out.println("come in call method");
+        return 1024;
+    }
+}
+
+public class CallableDemo {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+//        Thread thread = new Thread();
+        FutureTask<Integer> futureTask = new FutureTask(new MyThread());
+
+        new Thread(futureTask, "A").start();
+
+        Integer result = futureTask.get();
+        System.out.println(result);
+    }
+}
