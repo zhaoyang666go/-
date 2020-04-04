8cbfff9e5634e7a3c2500067c4aee617d25d18a5
zhaoyang666go <zhaoyang8216@sina.com>
2020-04-04 11:22:09 +0800

    类加载器

---
+++ src/main/java/zhaoyang/study/jvm/MyObject.java
@@ -0,0 +1,18 @@
+package zhaoyang.study.jvm;
+
+public class MyObject {
+    public static void main(String[] args) {
+        Object obj = new Object();  //系统自带的类
+//        System.out.println(obj.getClass().getClassLoader().getParent().getParent());    //异常
+//        System.out.println(obj.getClass().getClassLoader().getParent());    //异常
+        System.out.println(obj.getClass().getClassLoader());    //null  启动类加载器(Bootstrap)
+
+        System.out.println("***************");
+
+        MyObject myObj = new MyObject();
+        System.out.println(myObj.getClass().getClassLoader().getParent().getParent());  //null
+        System.out.println(myObj.getClass().getClassLoader().getParent());  //ExtClassLoader    扩展类加载器
+        System.out.println(myObj.getClass().getClassLoader());  //AppClassLoader 应用程序类加载器
+
+    }
+}
