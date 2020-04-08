f483b8f1c8a3d23813ad64d8673556240bdacc05
zhaoyang666go <zhaoyang8216@sina.com>
2020-04-08 20:48:24 +0800

    能够提高多线程效率的锁

---
+++ src/main/java/zhaoyang/study/concurrent/ReadWriteLockDemo.java
@@ -0,0 +1,68 @@
+package zhaoyang.study.concurrent;
+
+/*
+* 多个线程同时读一个资源类没有任何问题，为了满足并发量，读取共享资源应该可以同时进行
+* 但是，如果有一个线程要写共享资源类，就不应该有其它线程对该资源类进行读或写的操作
+* */
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+class MyCache {
+    private volatile Map<String, Object> map = new HashMap<>();
+    private ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+
+    public void put(String key, Object value){
+        readWriteLock.writeLock().lock();
+        try {
+            System.out.println(Thread.currentThread().getName() + "\t 开始写入 \t" + key);
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            map.put(key, value);
+            System.out.println(Thread.currentThread().getName() + "\t 写完了");
+        } finally {
+            readWriteLock.writeLock().unlock();
+        }
+    }
+
+    public void get(String key){
+        readWriteLock.readLock().lock();
+        try {
+            System.out.println(Thread.currentThread().getName() + "\t 开始读取");
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Object result = map.get(key);
+            System.out.println(Thread.currentThread().getName() + "\t 读取完成 \t" + result);
+        } finally {
+            readWriteLock.readLock().unlock();
+        }
+    }
+}
+
+public class ReadWriteLockDemo {
+    public static void main(String[] args) {
+        MyCache myCache = new MyCache();
+
+        for (int i=1; i<=5; i++) {
+            final int tempInt = i;
+            new Thread(() -> {
+                myCache.put(tempInt+"", tempInt+"");
+            }, String.valueOf(i)).start();
+        }
+
+        for (int i=1; i<=5; i++) {
+            final int tempInt = i;
+            new Thread(() -> {
+                myCache.get(tempInt+"");
+            }, String.valueOf(i)).start();
+        }
+    }
+}
