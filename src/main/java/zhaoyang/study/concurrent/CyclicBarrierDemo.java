01fe0c4194aecbc98a7053cf152d297abb50ed0e
zhaoyang666go <zhaoyang8216@sina.com>
2020-04-08 19:11:40 +0800

    JUC辅助类——CyclicBarrier（正数计时器）

---
+++ src/main/java/zhaoyang/study/concurrent/CyclicBarrierDemo.java
@@ -0,0 +1,29 @@
+package zhaoyang.study.concurrent;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+/*
+* CyclicBarrier 跟 CountDownLatch 正相反
+* Case：7个人开会，人都到期了才开始
+* */
+public class CyclicBarrierDemo {
+    public static void main(String[] args) {
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(7, () -> {
+            System.out.println("人到齐了，开会！");
+        });
+
+        for (int i=1; i<=7; i++) {
+            new Thread(() -> {
+                System.out.println(Thread.currentThread().getName() + "\t 到!");
+                try {
+                    cyclicBarrier.await();  //人没到齐就等待
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+            }, String.valueOf(i)).start();
+        }
+    }
+}
