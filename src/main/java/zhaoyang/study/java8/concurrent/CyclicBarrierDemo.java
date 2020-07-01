d8c6349e1831fc30c617d39df67c36d038fad149
zhaoyang666go <zhaoyang8216@sina.com>
2020-07-01 16:45:24 +0800

    CyclicBarrier演示新增集齐七龙珠召唤神龙

--- src/main/java/zhaoyang/study/java8/concurrent/CyclicBarrierDemo.java
+++ src/main/java/zhaoyang/study/java8/concurrent/CyclicBarrierDemo.java
@@ -1,14 +1,46 @@
 package zhaoyang.study.java8.concurrent;
 
+import lombok.Getter;
+
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.TimeUnit;
 
 /*
 * CyclicBarrier 跟 CountDownLatch 正相反
-* Case：7个人开会，人都到期了才开始
+* Case1：7个人开会，人都到期了才开始
+* Case2：集齐7颗龙珠，召唤神龙
 * */
 public class CyclicBarrierDemo {
     public static void main(String[] args) {
+//        meeting();
+
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(7, new Thread(() -> {
+            System.out.println("*******召唤神龙");
+        }));
+
+        for(int i=1; i<=7; i++){
+            final int temp = i;
+            new Thread(() -> {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(300);   //300ms的寻找时间
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName() + "\t 收集到" + temp + "星球");
+                try {
+                    cyclicBarrier.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+            }, NPCEnum.forEach_NPCEnum(i).getRetMessage()).start();
+        }
+    }
+
+    /*集齐7人开会*/
+    private static void meeting() {
         CyclicBarrier cyclicBarrier = new CyclicBarrier(7, () -> {
             System.out.println("人到齐了，开会！");
         });
@@ -26,5 +58,35 @@ public class CyclicBarrierDemo {
                 }
             }, String.valueOf(i)).start();
         }
+    }
+}
+
+enum NPCEnum{
+    ONE(1, "悟空"),
+    TWO(2, "克林"),
+    THREE(3, "悟饭"),
+    FOUR(4, "特南克斯"),
+    FIVE(5, "比克"),
+    SIX(6, "悟天"),
+    SEVEN(7, "贝吉塔");
+
+    @Getter
+    private Integer retCode;
+    @Getter
+    private String retMessage;
+
+    NPCEnum(Integer retCode, String retMessage) {
+        this.retCode = retCode;
+        this.retMessage = retMessage;
+    }
+
+    public static NPCEnum forEach_NPCEnum(int index){
+        NPCEnum[] npcArray = NPCEnum.values();
+        for(NPCEnum npc : npcArray){
+            if (index == npc.getRetCode()){
+                return npc;
+            }
+        }
+        return null;
     }
 }
