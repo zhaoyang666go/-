148cad3bbb40ee3ccd5ece48cddc2088e2863a3b
zhaoyang666go <zhaoyang8216@sina.com>
2020-07-06 22:28:23 +0800

    带JVM参数运行程序

---
+++ src/main/java/zhaoyang/study/java8/JVMParam/HelloGC.java
@@ -0,0 +1,21 @@
+package zhaoyang.study.java8.JVMParam;
+
+/**
+ * @author zhaoyang
+ * @Date 2020/7/6 - 15:27
+ *
+ * 测试带参数运行JVM
+ * IDEA集成环境设置参数并在终端查看
+ */
+public class HelloGC {
+    public static void main(String[] args) throws InterruptedException {
+        long totalMemory = Runtime.getRuntime().totalMemory();  //JVM内存总量
+        long maxMemory = Runtime.getRuntime().maxMemory();  //JVM尝试使用的最大内存量
+        System.out.println("Total-Memory: " + totalMemory + "字节、" + (totalMemory/(double)1024/1024) + "MB");
+        System.out.println("Max-Memory: " + maxMemory + "字节、" + (maxMemory/(double)1024/1024) + "MB");
+
+        System.out.println("******HelloGC");
+
+        Thread.sleep(Integer.MAX_VALUE);
+    }
+}
